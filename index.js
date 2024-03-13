a27eea28982cd4968f3353404966982c17505a54
SarkerPabon <sarkerpabon@gmail.com>
2024-03-14 04:14:18 +0600

    add default route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = 4000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	res.json({ message: "Hello World" });
+});
+
 app.get("/chart-one", async (req, res) => {
 	try {
 		const { answerOne, answerTwo } = await parseAnswersData();
